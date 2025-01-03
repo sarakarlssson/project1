729a4b0654613519d54af49f305ae91e694d446d
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-01-03 16:14:17 +0100

    Remove filen
    1. loopar igenom divarna för att hitta om div textcontent överrensstämmer med random number
    
    2. ta bort sifforna om de matchar med randomnumber

---
+++ common.js
@@ -0,0 +1,32 @@
+
+//skapa grid med random numbers
+let container = document.getElementById("container")
+let gridContainer = document.createElement("div");
+gridContainer.classList.add("gridContainer")
+let inputNumber = document.getElementById("inputNumber")
+container.appendChild(gridContainer)
+let createButton = document.getElementById("createButton")
+let newRandomNumberButton = document.getElementById("newRandomNumberButton")
+let newRandomNumberField = document.getElementById("newRandomNumberField")
+let arrayOfRandomNumbers = []
+
+function createRandomGrid(inputNumber) {
+    gridContainer.innerHTML = " "
+    arrayOfRandomNumbers = []
+
+    for (let i = 1; i <= inputNumber; i++) {
+        let div = document.createElement("div");
+        let randomNumber = Math.floor(Math.random() * 100);
+        div.textContent = randomNumber;
+        gridContainer.appendChild(div)
+        arrayOfRandomNumbers.push(randomNumber)
+    }
+}
+
+//skapar en ny grid med random nummer när du klickar på createknappen
+createButton.addEventListener("click", function () {
+    createRandomGrid(inputNumber.value)
+}
+)
+
+createRandomGrid(95)
---
+++ remove/index.js
@@ -0,0 +1,36 @@
+newRandomNumberButton.addEventListener("click", function () {
+    newRandomNumberField.value = Math.floor(Math.random() * 100)
+    let gridItems = document.querySelectorAll(".gridContainer div");
+    for (let i = 0; i < gridItems.length; i++) {
+
+        if (gridItems[i].textContent == newRandomNumberField.value) {
+            gridItems[i].style.backgroundColor = "orange"
+        }
+    }
+})
+
+let removeButton = document.getElementById("removeButton");
+let removeField = document.getElementById("removeField");
+
+removeButton.addEventListener("click", function () {
+    let gridItems = document.querySelectorAll(".gridContainer div");
+    let targetNumberToRemove = 0;
+    let isFound = false
+    for (let i = 0; i < gridItems.length; i++) {
+        if (gridItems[i].textContent == newRandomNumberField.value) {
+            removeField.value = gridItems[i].textContent + " removed "
+            gridItems[i].style.backgroundColor = "lightpink"
+            gridItems[i].textContent = "X"
+            targetNumberToRemove += 1;
+
+            isFound = true;
+        }
+    }
+    removeField.value += targetNumberToRemove + " times "
+
+    if (isFound == false) {
+        removeField.value = "NOT FOUND"
+    }
+
+
+})
