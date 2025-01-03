fddebf29125170f6429358a797e2c4fb93f215f2
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-01-03 16:20:57 +0100

    Remove
    ändrar så att färgen orange på random number ändras till grå efter varje nytt random number

--- remove/index.js
+++ remove/index.js
@@ -1,7 +1,13 @@
 newRandomNumberButton.addEventListener("click", function () {
+
+
     newRandomNumberField.value = Math.floor(Math.random() * 100)
     let gridItems = document.querySelectorAll(".gridContainer div");
+
     for (let i = 0; i < gridItems.length; i++) {
+        if (gridItems[i].style.backgroundColor == "orange") {
+            gridItems[i].style.backgroundColor = "lightgrey"
+        }
 
         if (gridItems[i].textContent == newRandomNumberField.value) {
             gridItems[i].style.backgroundColor = "orange"
