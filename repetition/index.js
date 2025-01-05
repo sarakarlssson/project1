e7188bc5fb5be77bbd7ff7940c2cb4fafa1500a4
sarakarlssson <sara.amanda.karlsson@gmail.com>
2025-01-05 14:34:45 +0100

    REPETITION
    skriver ut siffrorna som inte finns med från 0-100 och även de siffror som repeteras flest gånger

--- repetition/index.js
+++ repetition/index.js
@@ -1,37 +1,62 @@
-let mostRepeatedNumberField = document.getElementById("mostRepeatedNumberField")
-let numbersNotInPlaceField = document.getElementById("numbersNotInPlaceField")
-let gridItems = document.querySelectorAll(".gridContainer div");
+function repetition() {
 
-let numberCounter = 0;
-let compareNumber;
-let mostRepeatedNumber = -Infinity
-let theMostRepeatedNumber;
+    let mostRepeatedNumberField = document.getElementById("mostRepeatedNumberField")
+    let gridItems = document.querySelectorAll(".gridContainer div");
 
-for (let i = 0; i < gridItems.length; i++) {
-    compareNumber = gridItems[i].textContent
+    let numberCounter = 0;
+    let compareNumber;
+    let mostRepeatedNumber = -Infinity
+    let theMostRepeatedNumber;
 
-    for (let j = 0; j < gridItems.length; j++) {
-        if (gridItems[j].textContent == compareNumber) {
-            numberCounter++
+    for (let i = 0; i < gridItems.length; i++) {
+        compareNumber = gridItems[i].textContent
+
+        for (let j = 0; j < gridItems.length; j++) {
+            if (gridItems[j].textContent == compareNumber) {
+                numberCounter++
+            }
+        }
+        if (numberCounter > mostRepeatedNumber) {
+            mostRepeatedNumber = numberCounter
+            theMostRepeatedNumber = compareNumber
+            mostRepeatedNumberField.value = theMostRepeatedNumber
+        }
+        if (numberCounter == mostRepeatedNumber) {
+            mostRepeatedNumberField.value = theMostRepeatedNumber += ", " + compareNumber + "(x)"
+
+        }
+
+        numberCounter = 0;
+    }
+
+    let numbersNotInPlaceField = document.getElementById("numbersNotInPlaceField")
+    let numbersNotInGridArray = []
+    let isFound = false;
+
+    for (let i = 0; i <= 100; i++) {
+        isFound = false;
+        for (let j = 0; j < gridItems.length; j++) {
+            if (gridItems[j].textContent == i) {
+                isFound = true
+            }
+        }
+        if (isFound == false) {
+            numbersNotInGridArray.push(i)
         }
     }
-    if (numberCounter > mostRepeatedNumber) {
-        mostRepeatedNumber = numberCounter
-        theMostRepeatedNumber = compareNumber
-        mostRepeatedNumberField.value = theMostRepeatedNumber
-    }
-    if (numberCounter == mostRepeatedNumber) {
-        mostRepeatedNumber = numberCounter
-        theMostRepeatedNumber = compareNumber
-        if (compareNumber == theMostRepeatedNumber) {
-            mostRepeatedNumberField.value = theMostRepeatedNumber + " repeated " + numberCounter + " times"
+    numbersNotInPlaceField.value = numbersNotInGridArray
+    console.log(numbersNotInGridArray)
+
+    for (let i = 0; i < gridItems.length; i++) {
+        if (gridItems[i].textContent == mostRepeatedNumberField.value) {
+            console.log("tjenna")
         }
-        // if (compareNumber != theMostRepeatedNumber) {
-        //     mostRepeatedNumberField.value += ", " + theMostRepeatedNumber
-
-        // }
     }
-
-    numberCounter = 0;
 }
 
+
+createButton.addEventListener("click", function () {
+    repetition();
+})
+
+repetition();
